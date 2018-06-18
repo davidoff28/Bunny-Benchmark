1c804cf6e71c6efd384e5cfba416e974051b1ab8
davidoff28 <davidcundy2811@hotmail.co.uk>
2018-06-18 14:22:58 +0100

    First commit

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Example
+{
+    /// <summary>
+    /// This example is based on the bunnybenchmark
+    /// https://github.com/dmitryhryppa/frameworks_test/tree/master/bunnymark
+    /// </summary>
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            // this is required to find SFML.dll.
+            string dllDirectory = Path.GetFullPath("../../External");
+            Environment.SetEnvironmentVariable("PATH", 
+                Environment.GetEnvironmentVariable("PATH") + ";" 
+                + dllDirectory);            
+
+            // create and run app.
+            Application app = new Application(800, 600, "Benchmark");
+            app.Run();
+        }
+    }
+}
